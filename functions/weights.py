a7e0f5906bdcb2005a7c549a380b629d6c1079c7
Lukas Brunner <lukas.brunner@env.ethz.ch>
2018-09-19 15:45:31 +0200

    Rework all functions

--- functions/weights.py
+++ functions/weights.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Time-stamp: <2018-06-05 11:13:31 lukbrunn>
+Time-stamp: <2018-09-19 12:57:53 lukbrunn>
 
 (c) 2018 under a MIT License (https://mit-license.org)
 
@@ -13,132 +13,79 @@ Authors:
 Abstract:
 
 """
+import logging
 import numpy as np
 
+logger = logging.getLogger(__name__)
 
-def calc_wu(delta, sigma=.6):
-    """
-    Calculate weights accounting for dependence between models.
-
-    Parameters:
-    - delta (np.array): Shape (N,N), first dimension has to be 'models'
-    - sigma (list of float): Shape (M,)
-
-    Returns:
-    - np.array shape=(M,N) (squeezed if M=1)
-    """
-    if isinstance(sigma, float):
-        sigma = [sigma]
-    wu = np.empty((len(sigma), delta.shape[0])) * np.nan
-    for i_sig, sig in enumerate(sigma):
-        S = np.exp(-((delta/sig)**2))  # (N, N)
-        wu[i_sig] = [1. / (1 + np.sum(np.delete(ss, ii))) # (N, N)
-                           for ii, ss in enumerate(S)]  # sum over i!=j, (N)
-
-    return wu.squeeze()
-
-
-def calc_wq(delta, sigma=.6):
-    """
-    Calculate weights accounting for model quality.
+def calculate_weights(quality, independence, sigma_q, sigma_i):
+    """Calculates the weights for each model N.
 
     Parameters:
-    - delta (np.array): Shape (N,...), first dimension has to be 'models'
-    - sigma (list of float): Shape (M,)
+    - quality (np.array): (N,) array specifying the model quality. TODO: need to be normalized?
+    - independence (np.array): (N, N) array specifying the model independence. # TODO: need to be normalized?
+    - sigma_q (float): Sigma value defining the form of the weighting function
+      for the quality.
+    - sigma_i (float): Sigma value defining the form of the weighting function
+      for the independence.
 
     Returns:
-    - np.array shape=(M,N) (squeezed if M=1)
-    """
-    if isinstance(sigma, float):
-        sigma = [sigma]
-    wq = np.empty((len(sigma),) + np.shape(delta)) * np.nan
-    for i_sig, sig in enumerate(sigma):
-        wq[i_sig] = np.exp(-((delta/sig)**2))
+    np.array (N,)"""
+    if len(quality.shape) != 1 or len(independence.shape) != 2:
+        errmsg = 'quality and independence need to be 1D and 2D arrays, respectively'
+        logger.error(errmsg)
+        raise IOError(errmsg)
+    if quality.shape != independence.shape[:1]:
+        errmsg = 'quality and independence need have the same length'
+        logger.error(errmsg)
+        raise IOError(errmsg)
+    numerator = np.exp(-((quality/sigma_q)**2))
 
-    return wq.squeeze()
+    exp = np.exp(-((independence/sigma_i)**2))
+    sum_exp = [np.sum(np.delete(ee, ii)) for ii, ee in enumerate(exp)]  # sum i!=j
+    denominator = 1 + np.array(sum_exp)
+    return numerator/denominator
 
 
-def calc_weights_approx(wu, wq, data, std=False):
-    """
-    Calculate weights and approximation based on weights (weighted multi-model
-    mean)
+def calculate_weights_sigmas(data, distances, sigmas_q, sigmas_i):
+    """Calculates the weights for each model N and combination of sigma values.
+    Also calculates the weighted model mean for each combination on the fly.
 
     Parameters:
-    - wu (np.array): Shape (M,)
-    - wq (np.array): Shape (M,)
-    - data (np.array): Shape (N,...)
-    - std=False (bool, optional): If True the diagnostic is based on the
-    standard deviation, therefor the multi model mean is a mean of std which
-    cannot simply be calculated using mean
+    - data (np.array): (N,...) array of model data
+    - distances (np.array): (N, N) array specifying the distances between
+      each model.
+    - sigmas_q (np.array): (M,) array of possible sigma values for the
+      weighting function of the quality.
+    - sigmas_i (np.array): (L,) array of possible sigma values for the
+      weighting function of the independence.
 
     Returns:
-    - tuple of two np.array, weights has shape (N), approx same shape as data
-    """
-    def multiply_along_axis(arr, arr1D, axis):
-    # https://stackoverflow.com/questions/30031828/multiply-numpy-ndarray-with-1d-array-along-a-given-axis
-        dim_array = np.ones((1, arr.ndim), int).ravel()
-        dim_array[axis] = -1
-        arr1D_resh = arr1D.reshape(dim_array)
-        return arr * arr1D_resh
+    weights (M, L, N), weighted_mean (M, L,...)"""
+    if len(distances.shape) != 2:
+        errmsg = 'distances needs to be a 2D array'
+        logger.error(errmsg)
+        raise IOError(errmsg)
+    if distances.shape[0] != distances.shape[1]:
+        errmsg = 'distances needs to be of shape (N, N)'
+        logger.error(errmsg)
+        raise IOError(errmsg)
+    if data.shape[0] != distances.shape[0]:
+        errmsg = 'First dimensions of data and distances need to match'
+        logger.error(errmsg)
+        raise IOError(errmsg)
 
-    w_prod = np.empty((len(wq))) * np.nan
-    for j in range(len(wq)):
-        tmp_wu = wu[j]
-        w_prod[j] = wq[j] * tmp_wu
+    weights = np.empty((len(sigmas_q), len(sigmas_i)) + distances.shape) * np.nan
+    mean = np.empty((len(sigmas_q), len(sigmas_i), distances.shape[0])) * np.nan
+    for idx_q, sigma_q in enumerate(sigmas_q):
+        for idx_i, sigma_i in enumerate(sigmas_i):
+            for idx_d, dd in enumerate(distances):
+                # dd is the distance of each model to the idx_d-th model (='Truth')
+                ww = calculate_weights(dd, distances, sigma_q, sigma_i)
+                # exclude the 'True' model by setting the weight to zero
+                ww[idx_d] = 0.
+                ww /= ww.sum()  # DEBUG: normalize just to be comparable with old script
+                mean[idx_q, idx_i, idx_d] = np.average(data, weights=ww, axis=0)
+                weights[idx_q, idx_i, idx_d] = ww
 
-    wu_wq_sum = np.nansum(w_prod)
-
-    if wu_wq_sum != 0.:
-        weights =  w_prod / wu_wq_sum
-    else:  # how can this ever happen? if wq and wu are both very small (and numerically rounded to zero)
-        weights = w_prod * 0.0
-
-    if std:
-        approx = np.sqrt(
-            np.sum(multiply_along_axis(data**2, weights, axis = 0), axis = 0) /
-            np.nansum(weights))
-    else:
-        approx = (np.sum(multiply_along_axis(data, weights, axis = 0), axis = 0)
-            / np.nansum(weights))
-
-    return weights, approx
-
-
-def calc_weights_approx_multiple_sigmas(wu, wq, data, std = False):
-    """
-    Calculate weights and approximation based on weights (weighted multi-model
-    mean) for multiple sigma combinations, weights have shape (S1, S2, N)
-    and approx as well.
-
-    Parameters:
-    - wu, wq (np.array): Shape (S1, S2, N)
-    - data (np.array): Shape (N,...)
-    - std=False (bool, optional): If True the diagnostic is based on the
-    standard deviation, therefor the multi model mean is a mean of std which
-    cannot simply be calculated using mean
-
-    Returns:
-    - tuple of two np.array, weights has shape (S1, S2, N),
-      approx shape (S1, S2, N, ...) (... same as data)
-    """
-
-    if len(wq) == data.shape[0]:
-        # could be done with calc_weights_approx only
-        weights, approx = calc_weights_approx(wu, wq, data, std)
-    else:
-        approx = np.empty((len(wu), len(wq),) + np.shape(data)) * np.nan
-        # for each sigma combination, for each model as truth 1weight per model 
-        weights = np.empty((len(wu), len(wq), data.shape[0],
-                            data.shape[0])) * np.nan
-
-        for u in range(len(wu)):
-            for q in range(len(wq)):
-                for j in range(data.shape[0]):
-                    tmp_wu = wu[u, :].copy()
-                    tmp_wq = wq[q, j, :]
-                    tmp_wu[j] = 0.0
-                    tmp_wq[j] = 0.0
-                    #import ipdb ; ipdb.set_trace()
-                    weights[u, q, j, :], approx[u, q, j, :] = calc_weights_approx(tmp_wu, tmp_wq, data, std)
-
-    return weights, approx  #, std
+    return weights, mean
