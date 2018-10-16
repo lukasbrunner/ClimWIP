28ff15db194522edb74d1bbbe9c437c1af6f757e
Lukas Brunner <lukas.brunner@env.ethz.ch>
2018-10-16 16:05:30 +0200

    Update test to use new config file

--- tests/tests_version/run_tests.sh
+++ tests/tests_version/run_tests.sh
@@ -2,16 +2,20 @@
 
 case $1 in
     'reference')
-        if [ -z "$(ls -A data/reference/)" ]; then
-            rm -r data/reference/*  # clean up first
-        fi
-        ./../../model_weighting.py test_reference
+        # if ! [ -z "$(ls -A data/reference/)" ]; then
+        #     echo "Delting existing files..."
+        #     rm -r data/reference/*  # clean up first
+        #     echo "Delting existing files... DONE"
+        # fi
+        ./../../model_weighting.py test_reference -f configs/config_test_versions
         ;;
     'new')
-        if [ -z "$(ls -A data/new/)" ]; then
+        if ! [ -z "$(ls -A data/new/)" ]; then
+            echo "Delting existing files..."
             rm -r data/new/*  # cean up first
+            echo "Delting existing files... DONE"
         fi
-        ./../../model_weighting.py test_new
+        ./../../model_weighting.py test_new -f configs/config_test_versions
         ./test_output_change.py
         ;;
     *)
