Fix bug in target calculation without ref period

model_weighting.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Time-stamp: <2018-10-23 14:38:11 lukbrunn>
+Time-stamp: <2018-10-23 15:07:17 lukbrunn>
 
 (c) 2018 under a MIT License (https://mit-license.org)
 
@@ -283,13 +283,13 @@ def calc_target(fn, cfg):
                                           region=cfg.region,
                                           overwrite=cfg.overwrite)
 
+                target_hist = xr.open_dataset(filename_diag)
+                target_hist = target_hist.squeeze('time')
+                target[cfg.target_diagnostic] -= target_hist[cfg.target_diagnostic]
+
         except Exception as exc:
             logger.error('Exception at model: {}'.format(model_ensemble))
             raise exc
-
-        target_hist = xr.open_dataset(filename_diag)
-        target_hist = target_hist.squeeze('time')
-        target[cfg.target_diagnostic] -= target_hist[cfg.target_diagnostic]
 
         target['model_ensemble'] = xr.DataArray([model_ensemble], dims='model_ensemble')
         targets.append(target)
@@ -477,6 +477,8 @@ def calc_predictors(fn, cfg):
         logger.debug('Normalize data... DONE')
         diagnostics_all.append(diagnostics)
 
+        logger.info('Calculate diagnostics for {} {}... DONE'.format(
+            diagn, cfg.predictor_aggs[idx]))
 
         # --- optional plot output for consistency checks ---
         if cfg.plot:
@@ -559,7 +561,7 @@ def calc_sigmas(targets, delta_i, cfg, debug=False):
     # since we know that there is one model with 10 members, the larges element should be about
     # 10x the smallest one!
     sigmas_i = np.linspace(.1*tmp, 1.9*tmp, SIGMA_SIZE)
-    # sigmas_i = np.array([tmp])  # DEBUG
+    sigmas_i = np.array([tmp])  # DEBUG
 
     model_ensemble = targets['model_ensemble'].data
     models = [*map(lambda x: x.split('_')[0], model_ensemble)]