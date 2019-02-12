15e9e7fb0dea422be8b330ca1926504a42660d9e
Lukas Brunner <lukas.brunner@env.ethz.ch>
2019-02-12 15:58:59 +0100

    Update lineplot script

--- post_processing/lineplot_timeseries.py
+++ post_processing/lineplot_timeseries.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Time-stamp: <2018-12-01 14:15:35 lukas>
+Time-stamp: <2019-01-29 10:42:25 lukbrunn>
 
 (c) 2018 under a MIT License (https://mit-license.org)
 
@@ -17,30 +17,28 @@ import argparse
 import warnings
 import numpy as np
 import xarray as xr
-import datetime as dt
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
-import seaborn as sns
 from cdo import Cdo
-from tempfile import TemporaryDirectory
 cdo = Cdo()
-# sns.set()
 
 from utils_python import utils
 from utils_python.xarray import area_weighted_mean
-from utils_python.math import variance
+from utils_python.math import variance, quantile2
 
 from model_weighting.model_weighting import set_up_filenames, get_filenames
 from model_weighting.functions.diagnostics import calculate_basic_diagnostic
 
 REGION_DIR = '{}/../cdo_data/'.format(os.path.dirname(__file__))
 MASK = 'land_sea_mask_regionsmask.nc'  # 'seamask_g025.nc'
-SAVE_PATH = '/net/h2o/climphys/lukbrunn/Plots/ModelWeighting/eucp/lineplot_timeseries'
 
 variance = np.vectorize(variance, signature='(n)->()', excluded=['weights', 'biased'])
 period_ref = slice('1976', '2005')
 perfect_model_ensemble = 'MIROC5_r1i1p1'
+REGRID_OBS = [
+    'ERA-Interim',
+]
 
 
 def read_config():
@@ -54,13 +52,16 @@ def read_config():
     parser.add_argument(
         '--filename', '-f', dest='filename', default='../configs/config.ini',
         help='Relative or absolute path/filename.ini of the config file.')
+    parser.add_argument(
+        '--change', dest='change', action='store_true',
+        help='Plot change instead of absolute values')
     args = parser.parse_args()
     cfg = utils.read_config(args.config, args.filename)
     utils.log_parser(cfg)
-    return cfg
+    return cfg, args
 
 
-def read_data(cfg):
+def read_data(cfg, change):
     varn = cfg.target_diagnostic
     fn = set_up_filenames(cfg)
     ds_list = []
@@ -83,7 +84,10 @@ def read_data(cfg):
             ds = ds.resample(time='1a').mean()
         ds = area_weighted_mean(ds)
         ds = ds.sel(time=slice('1950', None))
-        # ds[varn] -= ds[varn].sel(time=period_ref).mean('time')
+        if change:
+            ds[varn] -= ds[varn].sel(time=slice(
+                str(cfg.target_startyear_ref),
+                str(cfg.target_endyear_ref))).mean('time')
         ds['model_ensemble'] = xr.DataArray([model_ensemble], dims='model_ensemble')
         ds['time'].data = ds['time'].dt.year.data
         ds_list.append(ds)
@@ -93,7 +97,7 @@ def read_data(cfg):
     return xr.concat(ds_list, dim='model_ensemble')
 
 
-def read_obs(cfg):
+def read_obs(cfg, change):
     varn = cfg.target_diagnostic
     path = cfg.obs_path
     source = cfg.obsdata
@@ -109,14 +113,17 @@ def read_obs(cfg):
         time_aggregation=None,
         mask_ocean=cfg.target_masko,
         region=cfg.region,
-        regrid=True)
+        regrid=cfg.target_diagnostic in REGRID_OBS)
 
     with warnings.catch_warnings():
         warnings.filterwarnings('ignore', message='Mean of empty slice')
         ds = ds.resample(time='1a').mean()
     ds = area_weighted_mean(ds)
     ds = ds.sel(time=slice('1950', None))
-    # ds[varn] -= ds[varn].sel(time=period_ref).mean('time')
+    if change:
+        ds[varn] -= ds[varn].sel(time=slice(
+            str(cfg.target_startyear_ref),
+            str(cfg.target_endyear_ref))).mean('time')
     ds['time'].data = ds['time'].dt.year.data
     return ds
 
@@ -128,11 +135,11 @@ def read_weights(model_ensemble, cfg):
     return ds['weights']
 
 
-def plot(cfg):
+def plot(cfg, args):
     varn = cfg.target_diagnostic
     fig, ax = plt.subplots()#figsize=(15, 5))
 
-    ds = read_data(cfg)
+    ds = read_data(cfg, change=args.change)
     data, xx = ds[varn].data, ds['time'].data
 
     l2 = None
@@ -140,12 +147,24 @@ def plot(cfg):
     # --- unweighted baseline ---
     # for dd in data:
     #     ax.plot(xx, dd, color='gray', alpha=.2, lw=.5)  # all lines
-    l1a = ax.fill_between(xx, data.mean(axis=0) - data.std(axis=0),  # StdDev shading
-                    data.mean(axis=0) + data.std(axis=0),
-                    color='gray', alpha=.2)
+    l1a = ax.fill_between(
+        xx,
+        quantile2(data, .05),
+        quantile2(data, .95),
+        # data.mean(axis=0) - data.std(axis=0),  # StdDev shading
+        # data.mean(axis=0) + data.std(axis=0),
+        color='gray',
+        alpha=.2,
+        zorder=100,
+    )
 
     # plot mean
-    [l1b] = ax.plot(xx, data.mean(axis=0), color='gray', lw=2)
+    [l1b] = ax.plot(
+        xx, data.mean(axis=0),
+        color='gray',
+        lw=2,
+        zorder=1000,
+    )
 
     # --- weighted ----
     weights = read_weights(ds['model_ensemble'].data, cfg)
@@ -159,51 +178,88 @@ def plot(cfg):
 
         # plot 'true' model
         [l2] = ax.plot(xx, ds.sel(model_ensemble=perfect_model_ensemble)[varn].data,
-                color='k', lw=2)
+                       color='k', lw=2)
 
     weights = weights.data
-    def color(ww):
-        """Different colors due to weights"""
-        if ww > .05:
-            return 'red'
-        elif ww > .01:
-            return 'orange'
-        elif ww > .005:
-            return 'yellow'
-        return 'gray'
+    # def color(ww):
+    #     """Different colors due to weights"""
+    #     if ww > .05:
+    #         return 'red'
+    #     elif ww > .01:
+    #         return 'orange'
+    #     elif ww > .005:
+    #         return 'yellow'
+    #     return 'gray'
     for dd, ww in zip(data, weights):
         ww /= weights.max()
-        ax.plot(xx, dd,
-                color='darkred', #color(ww),
-                alpha=ww,
-                lw=.5)  # all lines
+        ax.plot(
+            xx, dd,
+            color='darkred',  # color(ww),
+            alpha=ww,
+            lw=.3,
+            zorder=10,
+        )  # all lines
     [l3c] = ax.plot([], [], color='darkred', lw=.5)
-    l3a = ax.fill_between(xx,
-                    (np.average(data, weights=weights, axis=0) -
-                     np.sqrt(variance(data.swapaxes(0, 1), weights=weights))),
-                    (np.average(data, weights=weights, axis=0) +
-                     np.sqrt(variance(data.swapaxes(0, 1), weights=weights))),
-                          color='darkred', alpha=.2)
 
-    [l3b] = ax.plot(xx, np.average(data, weights=weights, axis=0), color='darkred', lw=2)
+    l3a = ax.fill_between(
+        xx,
+        quantile2(data, .05, weights),
+        quantile2(data, .95, weights),
+        # (np.average(data, weights=weights, axis=0) -
+        #  np.sqrt(variance(data.swapaxes(0, 1), weights=weights))),
+        # (np.average(data, weights=weights, axis=0) +
+        #  np.sqrt(variance(data.swapaxes(0, 1), weights=weights))),
+        color='darkred',
+        alpha=.2,
+        zorder=200,
+    )
+    [l3b] = ax.plot(
+        xx, np.average(data, weights=weights, axis=0),
+        color='darkred',
+        lw=2,
+        zorder=2000,
+    )
 
     # --- observations ---
-    obs = read_obs(cfg)
+    obs = read_obs(cfg, change=args.change)
     if obs is not None:
-        [l2] = ax.plot(obs['time'].data, obs[varn].data, color='k', lw=2)
+        [l2] = ax.plot(
+            obs['time'].data, obs[varn].data,
+            color='k',
+            lw=2,
+            zorder=3000,
+        )
 
     ax.set_xlabel('Year')
     ax.set_xlim(1950, 2060)
 
     # ax.set_ylabel('Temperature ($\degree$C)')
-    # ax.set_ylim(-5, 10)
+    if args.change:
+        ax.set_ylim(-4, 8)
+    else:
+        ax.set_ylim(10, 30)
 
-    ax.grid()
-    ax.set_title(varn)
+    ax.grid(zorder=0)
+    if cfg.target_diagnostic == 'tas':
+        varn = 'temperature ($\degree$C)'
+    elif cfg.target_diagnostic == 'pr':
+        varn = 'precipitation (mm/day)'
+    else:
+        varn = cfg.target_diagnostic
+
+    region = np.atleast_1d(cfg.region)
+    if 'NEU' in region and 'CEU' in region and 'MED' in region and len(region) == 3:
+        region = 'EU'
+    else:
+        region = ', '.join(region)
+    title = f'{region} {cfg.target_season} {varn}'
+    if args.change:
+        title += f' change ({cfg.target_startyear_ref}-{cfg.target_endyear_ref})'
+    ax.set_title(title)
 
     if l2 is None:
         plt.legend([(l1a, l1b), (l3a, l3b)],
-                   ['Unweighted mean & StdDev', 'Weighted mean & StdDev'],
+                   ['Unweighted mean & 90%', 'Weighted mean & 90%'],
                    loc='upper left')
 
     else:
@@ -211,25 +267,33 @@ def plot(cfg):
             [(l1a, l1b),
              (l3a, l3b),
              l2,
-             #l3c
+             # l3c
             ],
-            ['Unweighted mean & StdDev',
-             'Weighted mean & StdDev',
+            ['Unweighted mean & 90%',
+             'Weighted mean & 90%',
              'Observations',
-             #'Models by weight'
+             # 'Models by weight'
             ],
             loc='upper left'
         )
 
-    filename = os.path.join(SAVE_PATH, 'lines_{}'.format(cfg.config))
+    save_path = cfg.plot_path.replace('process_plots', 'timeseries')
+    filename = os.path.join(save_path, 'lines_{}'.format(cfg.config))
+    if args.change:
+        filename += '_change'
+
+    # filename = os.path.join(
+    #     '/home/lukbrunn/Documents/Conferences_etal/20190204_EUCP-GA/figures',
+    #     'tas_ceu_3')
+
     plt.savefig(filename + '.png', dpi=300)
     plt.savefig(filename + '.pdf', dpi=300)
     print('Saved: {}'.format(filename + '.png'))
 
 
 def main():
-    cfg = read_config()
-    plot(cfg)
+    cfg, args = read_config()
+    plot(cfg, args)
 
 
 if __name__ == '__main__':
