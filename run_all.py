b76b31f71eb62dc32dc3c5b2354fd556392e6b5f
Lukas Brunner <lukas.brunner@env.ethz.ch>
2019-03-20 14:57:30 +0100

    Update run script

--- run_all.py
+++ run_all.py
@@ -15,31 +15,39 @@ import argparse
 from datetime import datetime
 from configparser import ConfigParser
 
+DATA_PATH = '/net/h2o/climphys/lukbrunn/Data/ModelWeighting/EUCP/'
+PLOT_PATH = '/net/h2o/climphys/lukbrunn/Plots/ModelWeighting/'
+now = datetime.today().strftime('%Y%m%d_%H%M%S')
+
 parser = argparse.ArgumentParser()
 parser.add_argument(dest='filename', help='Relative path of the config file.')
 args = parser.parse_args()
 config = ConfigParser()
 config.read(args.filename)
 
-logname = os.path.basename(args.filename)
-with open('model_weighting.log', 'w') as logfile:
+config_name = os.path.basename(args.filename)
+logname = config_name.replace('.ini', f'_{now}.log')
+
+with open(f'logfiles/{logname}', 'w') as logfile:
     logfile.write('=' * 79)
-    logfile.write('\n{0} {1} {0}\n'.format('=' * 5, logname))
+    logfile.write('\n{0} {1} {0}\n'.format('=' * 5, config_name))
     logfile.write('=' * 79)
 
 for section in config.sections():
+    if config[section]['save_path'] is None:
+        config[section]['save_path'] = os.path.join(
+            DATA_PATH, os.path.basename(args.filename.replace('.ini', '')))
+    if config[section]['plot_path'] is None:
+        config[section]['plot_path'] = os.path.join(
+            PLOT_PATH, os.path.basename(args.filename.replace('.ini', '')), 'process_plots')
     os.makedirs(config[section]['save_path'], exist_ok=True)
     os.makedirs(config[section]['plot_path'], exist_ok=True)
     os.makedirs(config[section]['plot_path'].replace('process_plots', 'timeseries'), exist_ok=True)
-    with open('model_weighting.log', 'a') as logfile:
+    with open(f'logfiles/{logname}', 'a') as logfile:
         logfile.write('\n\n')
         logfile.write('-' * 79)
         logfile.write('\n{0} {1} {0}\n'.format('-' * 5, section))
         logfile.write('-' * 79)
         logfile.write('\n')
-    os.system(f'python model_weighting.py {section} -f {args.filename} -log-file model_weighting.log')
+    os.system(f'python model_weighting.py {section} -f {args.filename} -log-file logfiles/{logname}')
     os.system(f'python post_processing/lineplot_timeseries.py {section} -f ../{args.filename}')
-
-now = datetime.today().strftime('%Y%m%d_%H%M%S')
-logname = logname.replace('.ini', f'_{now}.log')
-os.rename('model_weighting.log', f'logfiles/{logname}')
