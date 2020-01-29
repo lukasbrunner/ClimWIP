5441d5ce83d1353c453190992cc30c82e89b4864
Lukas Brunner <lukas.brunner@env.ethz.ch>
2020-01-29 11:01:41 +0100

    Add some additional and easier checks

--- model_weighting/core/read_config.py
+++ model_weighting/core/read_config.py
@@ -18,6 +18,168 @@ from core import utils
 
 logger = logging.getLogger(__name__)
 
+types = {
+    # --- other parameters ---
+    'variants_use': (int, str),
+    'variants_select': str,
+    'variants_independence': bool,
+    'idx_lats': (int, type(None)),
+    'idx_lons': (int, type(None)),
+    'inside_ratio': (float, str, type(None)),
+    'overwrite': bool,
+    'percentiles': float,
+    'performance_metric': str,
+    'plot_path': (str, type(None)),
+    'plot': bool,
+    'subset': (str, type(None)),
+    'save_path': str,
+    'sigma_i': (int, float, type(None)),
+    'sigma_q': (int, float, type(None)),
+
+    # --- data ---
+    'model_path': str,
+    'model_id': str,
+    'model_scenario': str,
+    'obs_path': str,
+    'obs_id': str,
+    'obs_uncertainty': (str, type(None)),
+
+    # --- target ---
+    'target_diagnostic': (str, type(None)),
+    'target_agg': str,
+    'target_season': str,
+    'target_mask': (bool, str),
+    'target_region': str,
+    'target_startyear': int,
+    'target_endyear': int,
+    'target_startyear_ref': (int, type(None)),
+    'target_endyear_ref': (int, type(None)),
+
+    # --- performance ---
+    'performance_diagnostics': (str, type(None)),
+    'performance_aggs': (str, type(None)),
+    'performance_seasons': (str, type(None)),
+    'performance_masks': (bool, str, type(None)),
+    'performance_regions': (str, type(None)),
+    'performance_startyears': (int, type(None)),
+    'performance_endyears': (int, type(None)),
+    'performance_normalizers': (str, int, float, type(None)),
+    'performance_weights': (int, float, type(None)),
+
+    # --- independence ---
+    'independence_diagnostics': str,
+    'independence_aggs': str,
+    'independence_seasons': str,
+    'independence_masks': (bool, str),
+    'independence_regions': str,
+    'independence_startyears': int,
+    'independence_endyears': int,
+    'independence_normalizers': (str, int, float),
+    'independence_weights': (int, float, type(None)),
+}
+
+values = {
+    # --- other parameters ---
+    'idx_lats': None,
+    'idx_lons': None,
+    'inside_ratio': None,  # TODO
+    'overwrite': [True, False],
+    'percentiles': None,
+    'performance_metric': ['RMSE'],
+    'plot_path': None,  # TODO: writable
+    'plot': [True, False],
+    'subset': None,
+    'save_path': None,  # TODO: writable
+    'sigma_i': None,
+    'sigma_q': None,
+
+    # --- data ---
+    'model_path': None,  # TODO: exists
+    'model_id': ['CMIP3', 'CMIP5', 'CMIP6', 'LE'],
+    'model_scenario': None,  # TODO:
+    'variants_use': ['all', *list(range(1, 100))],
+    'variants_select': ['sorted', 'natsorted', 'random'],
+    'variants_independence': [True, False],
+    'obs_path': None,  # TODO: None or exists
+    'obs_id': None,
+    'obs_uncertainty': [None, 'range', 'mean', 'median', 'center'],
+
+    # --- target ---
+    'target_diagnostic': None,
+    'target_agg': ['CLIM'],
+    'target_season': ['ANN', 'JJA', 'SON', 'DJF', 'MAM'],
+    'target_mask': [False, 'sea', 'land'],
+    'target_region': None,   # TODO: check SREX or in folder
+    'target_startyear': range(1850, 2101),
+    'target_endyear': range(1850, 2101),
+    'target_startyear_ref': range(1850, 2101),
+    'target_endyear_ref': range(1850, 2101),
+
+    # --- performance ---
+    'performance_diagnostics': None,
+    'performance_aggs': ['ANOM-GLOBAL', 'ANOM-LOCAL', 'CLIM', 'STD', 'TREND'],
+    'performance_seasons': ['ANN', 'JJA', 'SON', 'DJF', 'MAM'],
+    'performance_masks': [False, 'sea', 'land'],
+    'performance_regions': None,  # TODO: check SREX or in folder
+    'performance_startyears': range(1850, 2020),
+    'performance_endyears': range(1850, 2020),
+    'performance_normalizers': None,
+    'performance_weights': None,
+
+    # --- independence ---
+    'independence_diagnostics': None,
+    'independence_aggs': ['ANOM-GLOBAL', 'ANOM-LOCAL', 'CLIM', 'STD', 'TREND'],
+    'independence_seasons': ['ANN', 'JJA', 'SON', 'DJF', 'MAM'],
+    'independence_masks': [False, 'sea', 'land'],
+    'independence_regions': None,  # TODO: check SREX or in folder
+    'independence_startyears': range(1850, 2020),
+    'independence_endyears': range(1850, 2020),
+    'independence_normalizers': None,
+    'independence_weights': None,
+}
+
+
+def check_types(cfg, types):
+    for param in types.keys():
+        if isinstance(cfg[param], list):
+            for value in cfg[param]:
+                if not isinstance(value, types[param]):
+                    errmsg = ' '.join([
+                        f'Wrong type in input field {param}! Each element',
+                        f'should be {" or ".join(types[param])} at least one',
+                        f'is {type(value)}!'])
+                    logger.error(errmsg)
+                    raise IOError(errmsg)
+        else:
+            if not isinstance(cfg[param], types[param]):
+                errmsg = ' '.join([
+                    f'Wrong type in input field {param}! Should be',
+                    f'{", ".join(types[param])} is {type(value)}!'])
+                logger.error(errmsg)
+                raise IOError(errmsg)
+
+
+def check_values(cfg, values):
+    for param in values.keys():
+        if values[param] is None:
+            continue
+        if isinstance(cfg[param], list):
+            for value in cfg[param]:
+                if value not in values[param]:
+                    errmsg = ' '.join([
+                        f'Wrong input value in field {param}! Each element',
+                        f'should be in {", ".join(values[param])} at least one',
+                        f'is not: {value}'])
+                    logger.error(errmsg)
+                    raise IOError(errmsg)
+        else:
+            if cfg[param] not in values[param]:
+                errmsg = ' '.join([
+                    f'Wrong input value in field {param}! Should be in',
+                    f'{", ".join(values[param])} but is: {value}'])
+                logger.error(errmsg)
+                raise IOError(errmsg)
+
 
 def set_default_values(cfg):
     try:
@@ -59,19 +221,19 @@ def set_default_values(cfg):
 
 def process_other_parameters(cfg):
     other_parameters = {
-        'variants_use': (int, str),
-        'variants_select': str,
-        'variants_independence': bool,
-        'idx_lats': (int, type(None)),
-        'idx_lons': (int, type(None)),
-        'inside_ratio': (float, str, type(None)),
-        'overwrite': bool,
-        'percentiles': float,
-        'performance_metric': str,
-        'plot_path': (str, type(None)),
-        'plot': bool,
-        'subset': (str, type(None)),
-        'save_path': str,
+        'variants_use',
+        'variants_select',
+        'variants_independence',
+        'idx_lats',
+        'idx_lons',
+        'inside_ratio',
+        'overwrite',
+        'percentiles',
+        'performance_metric',
+        'plot_path',
+        'plot',
+        'subset',
+        'save_path',
     }
 
     for param in other_parameters:
@@ -178,26 +340,32 @@ def process_obs_parameters(cfg):
         return  # all parameters contain a single None
 
     param = 'obs_path'
-    if not isinstance(cfg[param], list):
-        cfg[param] = [cfg[param]]
-    if not np.all([isinstance(value, obs_parameters[param])
-                   for value in cfg[param]]):
-        raise ValueError
-    size = len(cfg[param])
+    if cfg[param] is None:
+        cfg['obs_id'] = None
+    else:
+        if not isinstance(cfg[param], list):
+            cfg[param] = [cfg[param]]
+            if not np.all([isinstance(value, obs_parameters[param])
+                           for value in cfg[param]]):
+                raise ValueError
+        size = len(cfg[param])
 
     param = 'obs_id'
-    if not isinstance(cfg[param], list):
-        cfg[param] = [cfg[param]]
-    if not np.all([isinstance(value, obs_parameters[param])
-                   for value in cfg[param]]):
-        raise ValueError
-    if not len(cfg[param]) == size:
-        raise ValueError
+    if cfg[param] is None:
+        cfg['obs_path'] = None
+    else:
+        if not isinstance(cfg[param], list):
+            cfg[param] = [cfg[param]]
+        if not np.all([isinstance(value, obs_parameters[param])
+                       for value in cfg[param]]):
+            raise ValueError
+        if not len(cfg[param]) == size:
+            raise ValueError
 
     param = 'obs_uncertainty'
     if not isinstance(cfg[param], obs_parameters[param]):
         raise ValueError
-    if cfg[param] is None and len(cfg['obs_path']) != 1:
+    if cfg[param] is None and not (len(cfg['obs_path']) == 1 or cfg['obs_path'] is None):
         raise ValueError
 
 
@@ -214,9 +382,9 @@ def process_target_parameters(cfg):
         'target_endyear_ref': (int, type(None)),
     }
     if cfg.target_diagnostic is None:
-        for param in target_parameters.keys():
-            if cfg[param] is not None:
-                raise ValueError
+        # for param in target_parameters.keys():
+        #     if cfg[param] is not None:
+        #         raise ValueError
         return  # all parameters contain a single None
 
     for param in target_parameters.keys():
