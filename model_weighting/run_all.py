62b90b0213c68a8e33f79bb8a597ce3f1a0fc41a
Lukas Brunner <lukas.brunner@env.ethz.ch>
2020-01-30 11:49:32 +0100

    Add option to save to a different folder

--- model_weighting/run_all.py
+++ model_weighting/run_all.py
@@ -19,6 +19,8 @@ now = datetime.today().strftime('%Y%m%d_%H%M%S')
 
 parser = argparse.ArgumentParser()
 parser.add_argument(dest='filename', help='Relative path of the config file.')
+parser.add_argument('-p', dest='paper', action='store_true',
+                    help='Save to a dedicated folder for easier tracking')
 args = parser.parse_args()
 config = ConfigParser()
 config.read(args.filename)
@@ -26,16 +28,21 @@ config.read(args.filename)
 config_name = os.path.basename(args.filename)
 logname = config_name.replace('.ini', f'_{now}.log')
 
-with open(f'logfiles/{logname}', 'w') as logfile:
+if args.paper:
+    logname = os.path.join('logfiles_paper', logname)
+else:
+    logname = os.path.join('logfiles', logname)
+
+with open(logname, 'w') as logfile:
     logfile.write('=' * 79)
     logfile.write('\n{0} {1} {0}\n'.format('=' * 5, config_name))
     logfile.write('=' * 79)
 
 for section in config.sections():
-    with open(f'logfiles/{logname}', 'a') as logfile:
+    with open(logname, 'a') as logfile:
         logfile.write('\n\n')
         logfile.write('-' * 79)
         logfile.write('\n{0} {1} {0}\n'.format('-' * 5, section))
         logfile.write('-' * 79)
         logfile.write('\n')
-    os.system(f'nice python model_weighting_main.py {section} -f {args.filename} -log-file logfiles/{logname}')
+    os.system(f'nice python model_weighting_main.py {section} -f {args.filename} -log-file {logname}')
